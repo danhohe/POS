55f4f847d5ded9f70559cf8885a8517ca848ad6a
danhohe <dani.hoheneder@gmail.com>
2024-04-11 01:23:07 +0200

    added class "Word"

---
+++ 4thSemester/VocabTrainer/Word.cs
@@ -0,0 +1,36 @@
+﻿#nullable disable
+
+namespace VocabTrainer;
+
+public class Word
+{
+    #region fields
+    private string _germanWord;
+    private string _englishWord;
+    private int _hits;
+    private int _fails;
+    #endregion fields
+
+    #region properties
+    public string GermanWord
+    {
+        get{return _germanWord;}
+        set{_germanWord = value;}
+    }
+    public string EnglishWord
+    {
+        get{return _englishWord;}
+        set{_englishWord = value;}
+    }
+    public int Hits
+    {
+        get{return _hits;}
+        set{_hits = value;}
+    }
+    public int Fails
+    {
+        get{return _fails;}
+        set{_fails = value;}
+    }
+    #endregion properties
+}
