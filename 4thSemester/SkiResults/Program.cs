cb49a0052904df81ab4d7b3a7bff41e1f56f9d58
danhohe <dani.hoheneder@gmail.com>
2024-04-11 12:11:27 +0200

    added Menu

--- 4thSemester/SkiResults/Program.cs
+++ 4thSemester/SkiResults/Program.cs
@@ -1,13 +1,29 @@
 ﻿#nullable disable
+
+
 namespace SkiResults
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            RunApp();
             Console.WriteLine("press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static void RunApp()
+        {
+            PrintMenu();
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("1) Rangliste ausgeben");
+            Console.WriteLine("2) Diqualifizierte Läufer ausgeben");
+            Console.WriteLine("3) Disqualifizierte Läufer löschen");
+            Console.WriteLine("4) Ergebnis speichern");
+            Console.WriteLine("0) Programmende");
         }
     }
 }
