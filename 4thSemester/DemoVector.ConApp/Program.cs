03e62dbc858a94f8ffe43481ca46f34861cd4225
danhohe <dani.hoheneder@gmail.com>
2024-05-07 18:33:01 +0200

    added new project "DemoVector.ConApp"

---
+++ 4thSemester/DemoVector.ConApp/Program.cs
@@ -0,0 +1,28 @@
+﻿#nullable disable
+namespace DemoVector.ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a, b, c;
+            a = new Vector();
+            b = new Vector(3, 4);
+            c = new Vector(10, 20);
+
+            Console.WriteLine($"Vector: {a}");
+            Console.WriteLine($"Vector: {b}");
+            Console.WriteLine($"Vector: {c}");
+
+            c.Add(b);
+
+            Console.WriteLine($"Vector: {c}");
+
+            Vector d = Vector.Add(b, c);
+            Console.WriteLine($"Vector: {d}");
+
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4thSemester/DemoVector.ConApp/Vector.cs
@@ -0,0 +1,54 @@
+﻿namespace DemoVector.ConApp;
+
+public class Vector
+{
+    #region fields
+    private int _x;
+    private int _y;
+    #endregion fields
+
+    #region properties
+    public int X
+    {
+        get { return _x; }
+        set { _x = value; }
+    }
+    public int Y
+    {
+        get { return _y; }
+        set { _y = value; }
+    }
+    #endregion properties
+
+    #region constructors
+    public Vector()
+    {
+        _x = 0;
+        _y = 0;
+    }
+    public Vector(int x, int y)
+    {
+        _x = x;
+        _y = y;
+    }
+    #endregion constructors
+
+    #region overrides
+    public override string ToString()
+    {
+        return $"({X} | {Y})";
+    }
+    #endregion overrides
+
+    #region methods
+    public void Add(Vector vector)
+    {
+        this.X += vector._x;
+        this.Y += vector._y;
+    }
+    public static Vector Add(Vector a, Vector b)
+    {
+        return new Vector(a.X + b.X , a.Y + b.Y);
+    }
+    #endregion methods
+}
