09ec9cf4b7afd57410efec02d318fe4aaa10d762
danhohe <dani.hoheneder@gmail.com>
2024-05-14 17:21:20 +0200

    commit

---
+++ 4thSemester/FractionOne.ConApp/Fraction.cs
@@ -0,0 +1,97 @@
+﻿namespace FractionOne.ConApp;
+
+public class Fraction
+{
+    #region fields
+    private int _x;
+    private int _y;
+    #endregion fields
+
+    #region properties
+    public int X
+    {
+        get { return _x; }
+        set { _x = value; }
+    }
+    public int Y
+    {
+        get { return _y; }
+        set { _y = value; }
+    }
+    #endregion properties
+
+    #region constructors
+    public Fraction()
+    {
+        X = 0;
+        Y = 1;
+    }
+    public Fraction(int a, int b)
+    {
+        X = a;
+        Y = b;
+        if (b == 0)
+        {
+            Y = 1;
+        }
+    }
+    public Fraction(Fraction a)
+    {
+        X = a.X;
+        Y = a.Y;
+    }
+    public Fraction(int a)
+    {
+        X = a;
+        Y = 1;
+    }
+    #endregion constructors
+
+    #region operator overloadings
+
+    #endregion operator overloadings
+
+    #region overrides
+    public override string ToString()
+    {
+        return $"{X}/{Y}";
+    }
+    #endregion overrides
+
+    #region methods
+
+    public Fraction Add(int a)
+    {
+        X += Y * a;
+        return new Fraction(X, Y);
+    }
+    public Fraction Add(Fraction tmp)
+    {
+        if (Y != tmp.Y)
+        {
+            Y *= tmp.Y; 
+        }
+        X = X * Y + tmp.X;
+
+        return new Fraction(X, Y);
+    }
+    public double Value()
+    {
+        double result = 0;
+        result = (double)X / Y;
+        return result;
+    }
+    public Fraction Multiplier(int a)
+    {
+        X *= a;
+        return new Fraction(X, Y);
+    }
+    public Fraction Multiplier(Fraction tmp)
+    {
+        X *= tmp.X;
+        Y *= tmp.Y;
+        return new Fraction(X, Y);
+    }
+
+    #endregion methods
+}
---
+++ 4thSemester/FractionOne.ConApp/Program.cs
@@ -0,0 +1,33 @@
+﻿#nullable disable
+namespace FractionOne.ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction();     // Zähler:0, Nenner: 1
+            Fraction b = new Fraction(6, 4); // Zähler:6, Nenner: 4
+            Fraction c = new Fraction(5);    // Zähler:5, Nenner: 1
+            Fraction d = new Fraction(b);    // Zähler:6, Nenner: 4
+            Fraction e = new Fraction(3, 0); // Zähler:3, Nenner: 1, d.h. Nenner 0 ist nicht erlaubt
+
+            Console.WriteLine($"A: {a}");
+            Console.WriteLine($"B: {b}");
+            Console.WriteLine($"C: {c}");
+            Console.WriteLine($"D: {d}");
+            Console.WriteLine($"E: {e}");
+            Console.WriteLine();
+
+            Console.WriteLine("Wert von B: " + b.Value()); // Liefert den Wert des Objektes als float zurück
+                                          // (in unseren Fall 1,5)
+            Console.WriteLine();
+            Console.WriteLine("A: " + b.Add(3)); // ergibt für a den neuen Wert 18/4
+            Console.WriteLine("C: " + c.Add(d)); // ergibt für c den neuen Wert 26/4
+
+            Fraction f = b.Multiplier(c);
+            Console.WriteLine($"F: {f}");
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
