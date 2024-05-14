bfccd14fee4b0e3da4284d70472a34d47e5d15ac
danhohe <dani.hoheneder@gmail.com>
2024-05-14 18:12:09 +0200

    added new Demo project

---
+++ 4thSemester/DemoStack.ConApp/Stack.cs
@@ -0,0 +1,22 @@
+﻿namespace DemoStack.ConApp;
+
+public class Stack
+{
+    
+    #region properties
+    public bool IsEmpty
+    {
+        get{throw new NotImplementedException();}
+    }
+    #endregion properties
+    #region methods
+    public void Push(string data)
+    {
+        throw new NotImplementedException();
+    }
+    public string Pop()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion methods
+}
