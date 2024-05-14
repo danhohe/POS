b7fb1a5e7756b8af49db2bb5f27583836eb13784
danhohe <dani.hoheneder@gmail.com>
2024-05-15 01:58:42 +0200

    Added personal Testprojects

---
+++ SelfMadeProjects/TestUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace Program {
+    class Program {
+        private static void Main() {
+            System.Windows.Forms.Form mainForm = new System.Windows.Forms.Form();
+            System.Windows.Forms.Label lblFirst = new System.Windows.Forms.Label();
+            mainForm.Width = 300;
+            mainForm.Height = 400; 
+            lblFirst.Text = "Hello World";
+            lblFirst.Location = new System.Drawing.Point(150, 200);
+            mainForm.Controls.Add(lblFirst);
+            System.Windows.Forms.Application.Run(mainForm);
+        }   
+    }
+}
