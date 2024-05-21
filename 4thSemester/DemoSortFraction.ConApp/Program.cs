bc9ccd75bee26b6a84d6f472ab4ffb82184e41ad
danhohe <dani.hoheneder@gmail.com>
2024-05-21 17:48:35 +0200

    added DemoSortFraction.ConApp

---
+++ 4thSemester/DemoSortFraction.ConApp/Program.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+namespace DemoSortFraction.ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
--- SelfMadeProjects/TestUI/Program.cs
+++ SelfMadeProjects/TestUI/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Drawing;
-
+using WindowsForms;
 namespace Program {
     class Program {
         private static void Main() {
