42ee5a1a117c16b778e60ebcedcd605f6fd78dd3
danhohe <dani.hoheneder@gmail.com>
2024-05-24 09:59:35 +0200

    commit

--- 4thSemester/DemoVector.ConApp/obj/Debug/net8.0/DemoVector.ConApp.AssemblyInfo.cs
+++ 4thSemester/DemoVector.ConApp/obj/Debug/net8.0/DemoVector.ConApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoVector.ConApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51d119832eaa5dd09dcc5b1a0d2b8fd0a491b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoVector.ConApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoVector.ConApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Test/TestApp/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/TestApp.AssemblyInfo.cs
+++ Test/TestApp/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f92e8559cd68d08d33bd6a5c3346cd6a3e25b2e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b51d119832eaa5dd09dcc5b1a0d2b8fd0a491b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
