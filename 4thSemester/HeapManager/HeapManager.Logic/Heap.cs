53e7c4d50b1f46348901bb0d4d4aa0e783f9e68e
danhohe <dani.hoheneder@gmail.com>
2024-06-18 21:19:18 +0200

    added new project

---
+++ 4thSemester/HeapManager/HeapManager.Logic/Heap.cs
@@ -0,0 +1,11 @@
+﻿namespace HeapManager.Logic
+{
+    /// <summary>
+    /// Represents a heap data structure that manages memory blocks.
+    /// </summary>
+    public class Heap
+    {
+        
+    }
+}
+
