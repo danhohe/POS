553949dbd5f9c3ef859785f5bf368dd3a08df5d1
danhohe <dani.hoheneder@gmail.com>
2025-01-07 17:35:43 +0100

    init commit

---
+++ 5thSemester/ProductTrader/ProductTrader.ConApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ProductTrader.ConApp
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(/*string[] args*/)
+        {
+        }
+
+        /// <summary>
+        /// Prints the header information when the product changes.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The event data.</param>
+        static void PrintHeader(object sender, EventArgs e)
+        {
+            Console.Clear();
+            Console.WriteLine("Product-Trader");
+            Console.WriteLine();
+        }
+    }
+}
